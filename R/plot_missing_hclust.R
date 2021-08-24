91c6ae35d217570a2427e4316b6b78192212fe92
mattsq <mattsimmons@qantas.com.au>
2021-08-24 12:02:34 +1000

    Handling zero correlation properly and adding the heirarchical clustering method.

--- R/plot_missing_hclust.R
+++ R/plot_missing_hclust.R
@@ -1,12 +1,12 @@
-#' Plot Missing Correlation
+#' Plot Missing Correlation Clustering
 #'
 #' XXXX
 #'
 #' XXX
 #' @export
-#' @import dplyr corrplot
+#' @import dplyr
 #' @param data a XXX
-#' @param ... any arguments to corrplot::corrplot
+#' @param ... any arguments to stats::hclust
 #' @examples
 #' \dontrun{
 #'
@@ -15,10 +15,13 @@
 #' XXX
 #' @author Matt Simmons matthew.joseph.simmons@gmail.com
 #'
+#'
 
-plot_missing_correl <- function(data, ...) {
+plot_missing_hclust <- function(data, distance = "binary", ...) {
   data <- dplyr::mutate(data, dplyr::across(.cols = dplyr::everything(), ~ dplyr::if_else(is.na(.x), 1, 0)))
   data_cor <- stats::cor(data, method = "spearman")
   data_cor <- data_cor[is.na(data_cor)] <- 0
-  corrplot::corrplot(data_cor, ...)
+  data_dist <- stats::dist(data_cor, method = distance)
+  clust <- stats::hclust(data_dist, ...)
+  plot(clust)
 }
